6123af943dffb3e9227a040c4aca211d8d0b6afa
Szpori <usunesezawsze@gmail.com>
2025-05-09 13:21:27 +0000

    Updated deployment with Cloud SQL Proxy and service configuration

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ const server = http.createServer((req, res) => {
   res.end('Hello from Node.js HTTP server! Updated 5\n');
 });
 
+const { Client } = require('pg');
+
+const client = new Client({
+  host: '127.0.0.1',
+  port: 5432,
+  user: 'postgres',
+  password: 'myrootpassword',
+  database: 'my-database',
+});
+
+client.connect()
+  .then(() => console.log('Connected to DB'))
+  .catch(err => console.error('Connection error', err.stack));
+
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
